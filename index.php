e0fd62b7b42e06c61ffd0978800c605adf8e4de5
JC Coles <jcoles1155@gmail.com>
2014-09-17 20:49:02 -0700

    HTML code for project 1

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>JC Coles P1 - DWA15 Portfolio</title>
+	<meta charset="utf-8">
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel='stylesheet' href='//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css' type='text/css'>
+	<link href="//netdna.bootstrapcdn.com/bootswatch/3.1.1/flatly/bootstrap.min.css" rel="stylesheet">
+	
+	<style>
+	
+		h1 {
+			text-align:center;
+		}
+	
+		.container {
+			width:50%;
+			min-width:600px;
+			margin-bottom:50px;
+		}	
+		
+		img {
+			display:block;
+			margin:auto;
+			width:50%;
+		}
+	
+	</style>
+
+	
+</head>
+<body>
+
+
+	<div class="container">
+	
+		<h1>JC Coles <br> Dynamic Web Apps Portfolio</h1>
+		
+		<a href="http://imgur.com/XEvsSTT"><img src="http://i.imgur.com/XEvsSTT.jpg" title="Hosted by imgur.com" /></a>
+		
+		<h3>Project 2 Title</h3>
+		View: [link]
+		Github: [link]
+		
+		<h3>Project 3 Title</h3>
+		View: [link]
+		Github: [link]
+		
+		<h3>Project 4 Title</h3>
+		View: [link]
+		Github: [link]
+
+
+		
+		
+		
+		
+		<h2>In 2+ sentences describe who you are and what you do.</h2>
+		<p>My name is JC Coles and I am currently living in ventura California.  I grew up in a town about 2 hours northwest of here called Bakersfield and was a very active child growing up.  I played high school baseball and was an honor roll student all throughout high school while participating in such things as school associated body, and was the President of my class Jr. year.  I am highly motivated to achieve good grades especially in this class to hopefully get a job soon in computer programming.</p>
+		
+		<h2>Why are you taking this course?</h2>
+		<p>I am taking this course and investing time into the certification process because it seems like the most expedient path to a job in computer science.  I am also studying web tech. because of the foreseeable growth in online activity in the years to come.</p>
+		
+		<h2>What previous experience do you have with programming?</h2>
+		<p>The experience I have with programming is as follows: a class administered at a university on Java, time spent online studying HTML and CSS, and a class that I am currently enrolled in that is an introductory class in python. </p>
+		
+		<h2>Are you working on a Mac, PC, or some other platform?</h2>
+		<p>I am working on a MacBook Pro.</p>
+		
+		<h2>Anything else we should know?</h2>
+		<p>I am a highly motivated student and will do anything it takes to get a good grade in this class, and to learn as much as possible.</p>
+	
+	</div>
+	
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+	
+</body>
+</html>
